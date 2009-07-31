995b98e0c72a8571d3c1f72c470c6ed4f9b068d4
Yuri Baburov <burchik@gmail.com>
2009-07-31 10:43:38 +0700

    No more accepting filters in queryset utility functions.

--- queries.py
+++ queries.py
@@ -1,48 +1,47 @@
 from utils.commons.namedtuple import namedtuple
 
-def qs_filter(model_or_queryset, **filters):
-    if isinstance(model_or_queryset, (list, tuple)):
+def qs_filter(morqs, **filters):
+    """
+    morqs = model or queryset
+    """
+    if isinstance(morqs, (list, tuple)):
         if filters:
             raise Exception("Can't apply filters to a list")
-        return model_or_queryset
-    if hasattr(model_or_queryset, 'model'):
-        qs = model_or_queryset
+        return morqs
+    if hasattr(morqs, 'model'):
+        qs = morqs
     else:
-        qs = model_or_queryset.objects.all()
+        qs = morqs.objects.all()
     if filters:
         return qs.filter(**filters)
     else:
         return qs
 
-def as_list(model_or_queryset, col, **filters):
-    qs = qs_filter(model_or_queryset, **filters)
+### QuerySet utility functions
+def as_list(qs, col):
     return [x[col] for x in qs.values(col)]
 
-def as_set(model_or_queryset, col, **filters):
-    return set(as_list(model_or_queryset, col, **filters))
+def as_set(qs, col):
+    return set(as_list(qs, col))
 
-def as_dict(model_or_queryset, col_key, col_value, **filters):
-    qs = qs_filter(model_or_queryset)
+def as_dict(qs, col_key, col_value):
     return dict([(x[col_key], x[col_value]) for x in qs.values(col_key, col_value)])
 
-def as_map(model_or_queryset, key, **filters):
-    qs = qs_filter(model_or_queryset)
+def as_map(qs, key):
     return dict([(getattr(x, key), x) for x in qs])
 
-def as_map_tuples(model_or_queryset, key, **filters):
+### ValueQuerySet utility functions
+def as_map_tuples(vqs, key):
     """ Useful after queryset.values() """
-    qs = qs_filter(model_or_queryset)
-    return dict([(getattr(x, key), x) for x in as_tuples_iter(qs)])
+    return dict([(getattr(x, key), x) for x in as_tuples_iter(vqs)])
 
-def as_tuples(model_or_queryset, cols, **filters):
+def as_tuples(vqs, cols):
     """ Useful after queryset.values() """
-    qs = qs_filter(model_or_queryset)
     Row = namedtuple('Row', cols)
-    return [Row(*[row[key] for key in cols]) for row in qs]
+    return [Row(*[row[key] for key in cols]) for row in vqs]
 
-def as_tuples_iter(model_or_queryset, cols, **filters):
+def as_tuples_iter(vqs, cols):
     """ Useful after queryset.values() """
-    qs = qs_filter(model_or_queryset, **filters)
     Row = namedtuple('Row', cols)
-    for row in qs:
+    for row in vqs:
         yield Row(*[row[key] for key in cols])
