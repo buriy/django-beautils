cc0e421726f30b3c43b72e28a5eac6c65a5ed5be
Yuri Baburov <burchik@gmail.com>
2010-08-26 00:47:34 +0700

    Returned back proper staff_required function.

--- beautils/renderer.py
+++ beautils/renderer.py
@@ -58,6 +58,16 @@ def login_required2(function=None, redirect_field_name=REDIRECT_FIELD_NAME):
     return actual_decorator
 
 
+def staff_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME):
+    actual_decorator = user_passes_test(
+        lambda u: u.is_authenticated() and u.is_active and u.is_superuser,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+
 def not_authenticated(function=None, redirect_field_name=REDIRECT_FIELD_NAME):
     actual_decorator = user_passes_test(
         lambda u: not u.is_authenticated(),
